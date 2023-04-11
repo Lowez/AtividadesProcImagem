9df8dcc8f862d895a30944d51b1f01400eec9c97
Lowez <luizgtadisarz@gmail.com>
2023-04-10 21:44:35 -0300

    first commit

---
+++ AtividadesProcImagem/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace AtividadesProcImagem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.load1 = new System.Windows.Forms.Button();
+            this.load2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.adicao = new System.Windows.Forms.Button();
+            this.subtracao = new System.Windows.Forms.Button();
+            this.multiplicacao = new System.Windows.Forms.Button();
+            this.divisao = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(38, 90);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(203, 198);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // load1
+            // 
+            this.load1.Location = new System.Drawing.Point(84, 305);
+            this.load1.Name = "load1";
+            this.load1.Size = new System.Drawing.Size(109, 30);
+            this.load1.TabIndex = 2;
+            this.load1.Text = "Carregar Imagem 1";
+            this.load1.UseVisualStyleBackColor = true;
+            this.load1.Click += new System.EventHandler(this.load1_Click_1);
+            // 
+            // load2
+            // 
+            this.load2.Location = new System.Drawing.Point(339, 305);
+            this.load2.Name = "load2";
+            this.load2.Size = new System.Drawing.Size(109, 30);
+            this.load2.TabIndex = 4;
+            this.load2.Text = "Carregar Imagem 2";
+            this.load2.UseVisualStyleBackColor = true;
+            this.load2.Click += new System.EventHandler(this.load2_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(293, 90);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(203, 198);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.InitialDirectory = "C:\\Users\\Computação\\Downloads\\MaterialMatlab\\Matlab";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // adicao
+            // 
+            this.adicao.Location = new System.Drawing.Point(554, 90);
+            this.adicao.Name = "adicao";
+            this.adicao.Size = new System.Drawing.Size(50, 45);
+            this.adicao.TabIndex = 5;
+            this.adicao.Text = "+";
+            this.adicao.UseVisualStyleBackColor = true;
+            this.adicao.Click += new System.EventHandler(this.adicao_Click);
+            // 
+            // subtracao
+            // 
+            this.subtracao.Location = new System.Drawing.Point(554, 157);
+            this.subtracao.Name = "subtracao";
+            this.subtracao.Size = new System.Drawing.Size(50, 45);
+            this.subtracao.TabIndex = 6;
+            this.subtracao.Text = "-";
+            this.subtracao.UseVisualStyleBackColor = true;
+            // 
+            // multiplicacao
+            // 
+            this.multiplicacao.Location = new System.Drawing.Point(554, 226);
+            this.multiplicacao.Name = "multiplicacao";
+            this.multiplicacao.Size = new System.Drawing.Size(50, 45);
+            this.multiplicacao.TabIndex = 7;
+            this.multiplicacao.Text = "*";
+            this.multiplicacao.UseVisualStyleBackColor = true;
+            // 
+            // divisao
+            // 
+            this.divisao.Location = new System.Drawing.Point(554, 288);
+            this.divisao.Name = "divisao";
+            this.divisao.Size = new System.Drawing.Size(50, 45);
+            this.divisao.TabIndex = 8;
+            this.divisao.Text = "/";
+            this.divisao.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(659, 90);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(203, 198);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(664, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 29);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Resultado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(48, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 29);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Imagem 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(302, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 29);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Imagem 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1046, 530);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.divisao);
+            this.Controls.Add(this.multiplicacao);
+            this.Controls.Add(this.subtracao);
+            this.Controls.Add(this.adicao);
+            this.Controls.Add(this.load2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.load1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button load1;
+        private System.Windows.Forms.Button load2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button adicao;
+        private System.Windows.Forms.Button subtracao;
+        private System.Windows.Forms.Button multiplicacao;
+        private System.Windows.Forms.Button divisao;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ AtividadesProcImagem/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AtividadesProcImagem
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap img1;
+        byte[,] vImg1Gray;
+        byte[,] vImg1R;
+        byte[,] vImg1G;
+        byte[,] vImg1B;
+        byte[,] vImg1A;
+
+        Bitmap img2;
+        byte[,] vImg2Gray;
+        byte[,] vImg2R;
+        byte[,] vImg2G;
+        byte[,] vImg2B;
+        byte[,] vImg2A;
+
+        private void load1_Click_1(object sender, EventArgs e)
+        {
+            // Configurações iniciais da OpenFileDialogBox
+            openFileDialog1 = new OpenFileDialog();
+            var filePath = string.Empty;
+            openFileDialog1.InitialDirectory = "C:\\Users\\Computação\\Downloads\\MaterialMatlab\\Matlab";
+            openFileDialog1.Filter = "TIFF image (*.tif)|*.tif|JPG image (*.jpg)|*.jpg|BMP image (*.bmp)|*.bmp|PNG image (*.png)|*.png|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            // Se um arquivo foi localizado com sucesso...
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Armnazena o path do arquivo de imagem
+                filePath = openFileDialog1.FileName;
+
+
+                bool bLoadImgOK = false;
+                try
+                {
+                    img1 = new Bitmap(filePath);
+                    bLoadImgOK = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Erro ao abrir imagem...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    bLoadImgOK = false;
+                }
+
+                // Se a imagem carregou perfeitamente...
+                if (bLoadImgOK == true)
+                {
+                    // Adiciona imagem na PictureBox
+                    pictureBox1.Image = img1;
+                    vImg1Gray = new byte[img1.Width, img1.Height];
+                    vImg1R = new byte[img1.Width, img1.Height];
+                    vImg1G = new byte[img1.Width, img1.Height];
+                    vImg1B = new byte[img1.Width, img1.Height];
+                    vImg1A = new byte[img1.Width, img1.Height];
+
+                    // Percorre todos os pixels da imagem...
+                    for (int i = 0; i < img1.Width; i++)
+                    {
+                        for (int j = 0; j < img1.Height; j++)
+                        {
+                            Color pixel = img1.GetPixel(i, j);
+
+                            // Para imagens em escala de cinza, extrair o valor do pixel com...
+                            //byte pixelIntensity = Convert.ToByte((pixel.R + pixel.G + pixel.B) / 3);
+                            byte pixelIntensity = Convert.ToByte((pixel.R + pixel.G + pixel.B) / 3);
+                            vImg1Gray[i, j] = pixelIntensity;
+
+                            // Para imagens RGB, extrair o valor do pixel com...
+                            byte R = pixel.R;
+                            byte G = pixel.G;
+                            byte B = pixel.B;
+                            byte A = pixel.A;
+
+                            vImg1R[i, j] = R;
+                            vImg1G[i, j] = G;
+                            vImg1B[i, j] = B;
+                            vImg1A[i, j] = A;
+
+                        }
+                    }
+                }
+            }
+            // pictureBox1.Load("C:\\Users\\Computação\\Downloads\\Material Matlab\\Matlab\\Add1.jpg");
+        }
+
+        private void load2_Click_1(object sender, EventArgs e)
+        {
+            // Configurações iniciais da OpenFileDialogBox
+            openFileDialog2 = new OpenFileDialog();
+            var filePath = string.Empty;
+            openFileDialog2.InitialDirectory = "C:\\Users\\Computação\\Downloads\\MaterialMatlab\\Matlab";
+            openFileDialog2.Filter = "TIFF image (*.tif)|*.tif|JPG image (*.jpg)|*.jpg|BMP image (*.bmp)|*.bmp|PNG image (*.png)|*.png|All files (*.*)|*.*";
+            openFileDialog2.FilterIndex = 2;
+            openFileDialog2.RestoreDirectory = true;
+
+            // Se um arquivo foi localizado com sucesso...
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                // Armnazena o path do arquivo de imagem
+                filePath = openFileDialog2.FileName;
+
+
+                bool bLoadImgOK = false;
+                try
+                {
+                    img2 = new Bitmap(filePath);
+                    bLoadImgOK = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Erro ao abrir imagem...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    bLoadImgOK = false;
+                }
+
+                // Se a imagem carregou perfeitamente...
+                if (bLoadImgOK == true)
+                {
+                    // Adiciona imagem na PictureBox
+                    pictureBox2.Image = img2;
+                    vImg2Gray = new byte[img2.Width, img2.Height];
+                    vImg2R = new byte[img2.Width, img2.Height];
+                    vImg2G = new byte[img2.Width, img2.Height];
+                    vImg2B = new byte[img2.Width, img2.Height];
+                    vImg2A = new byte[img2.Width, img2.Height];
+
+                    // Percorre todos os pixels da imagem...
+                    for (int i = 0; i < img2.Width; i++)
+                    {
+                        for (int j = 0; j < img2.Height; j++)
+                        {
+                            Color pixel = img2.GetPixel(i, j);
+
+                            // Para imagens em escala de cinza, extrair o valor do pixel com...
+                            //byte pixelIntensity = Convert.ToByte((pixel.R + pixel.G + pixel.B) / 3);
+                            byte pixelIntensity = Convert.ToByte((pixel.R + pixel.G + pixel.B) / 3);
+                            vImg2Gray[i, j] = pixelIntensity;
+
+                            // Para imagens RGB, extrair o valor do pixel com...
+                            byte R = pixel.R;
+                            byte G = pixel.G;
+                            byte B = pixel.B;
+                            byte A = pixel.A;
+
+                            vImg2R[i, j] = R;
+                            vImg2G[i, j] = G;
+                            vImg2B[i, j] = B;
+                            vImg2A[i, j] = A;
+
+                        }
+                    }
+                }
+            }
+            // pictureBox1.Load("C:\\Users\\Computação\\Downloads\\Material Matlab\\Matlab\\Add1.jpg");
+        }
+
+        private void adicao_Click(object sender, EventArgs e)
+        {
+            Bitmap addImage = (Bitmap)pictureBox1.Image;
+            Bitmap image1 = (Bitmap)pictureBox1.Image;
+            Bitmap image2 = (Bitmap)pictureBox2.Image;
+
+            int x, y;
+
+            for (x = 0; x < addImage.Width; x++)
+            {
+                for (y = 0; y < addImage.Height; y++)
+                {
+                    //vImg1Gray[x, y] = pixelIntensity;
+                    //vImg2Gray[x, y] = pixelIntensity2;
+                    Console.WriteLine(vImg2Gray[x, y]);
+
+                    //addImage.SetPixel(x, y, Convert.ToColor)
+
+                    //Color pixelColor = (image1.GetPixel(x, y));
+                    //Color newColor = Color.FromArgb(pixelColor.R, 0, 0);
+                    //addImage.SetPixel(x, y, newColor);
+                }
+            }
+
+            // pictureBox3.Image = (Bitmap)pictureBox1.Image + (Bitmap)pictureBox2.Image;
+        }
+    }
+}
